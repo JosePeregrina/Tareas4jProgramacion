fa227915a773aadf7e8e4bf014ac82915ddbfe44
Cheliz <jperegrina2@ucol.mx>
2018-02-22 22:27:12 -0600

    Proyecto tablero

---
+++ Tarea1TableroCarro/Tarea1TableroCarro/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Tarea1TableroCarro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAccion = new System.Windows.Forms.Button();
+            this.txtdato = new System.Windows.Forms.TextBox();
+            this.txtNuevaFecha = new System.Windows.Forms.TextBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.pbSensor1 = new System.Windows.Forms.PictureBox();
+            this.pbSensor2 = new System.Windows.Forms.PictureBox();
+            this.pbTanque = new System.Windows.Forms.PictureBox();
+            this.pbBrujula = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.cmdCambiarFecha = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSensor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSensor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTanque)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrujula)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdAccion
+            // 
+            this.cmdAccion.Location = new System.Drawing.Point(201, 33);
+            this.cmdAccion.Name = "cmdAccion";
+            this.cmdAccion.Size = new System.Drawing.Size(75, 23);
+            this.cmdAccion.TabIndex = 1;
+            this.cmdAccion.Text = "Acción";
+            this.cmdAccion.UseVisualStyleBackColor = true;
+            this.cmdAccion.Click += new System.EventHandler(this.cmdAccion_Click);
+            // 
+            // txtdato
+            // 
+            this.txtdato.Location = new System.Drawing.Point(47, 36);
+            this.txtdato.Name = "txtdato";
+            this.txtdato.Size = new System.Drawing.Size(115, 20);
+            this.txtdato.TabIndex = 3;
+            // 
+            // txtNuevaFecha
+            // 
+            this.txtNuevaFecha.Location = new System.Drawing.Point(81, 439);
+            this.txtNuevaFecha.Name = "txtNuevaFecha";
+            this.txtNuevaFecha.Size = new System.Drawing.Size(100, 20);
+            this.txtNuevaFecha.TabIndex = 5;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(47, 337);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 7;
+            // 
+            // pbSensor1
+            // 
+            this.pbSensor1.Location = new System.Drawing.Point(12, 92);
+            this.pbSensor1.Name = "pbSensor1";
+            this.pbSensor1.Size = new System.Drawing.Size(131, 141);
+            this.pbSensor1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSensor1.TabIndex = 2;
+            this.pbSensor1.TabStop = false;
+            // 
+            // pbSensor2
+            // 
+            this.pbSensor2.Location = new System.Drawing.Point(190, 92);
+            this.pbSensor2.Name = "pbSensor2";
+            this.pbSensor2.Size = new System.Drawing.Size(131, 141);
+            this.pbSensor2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSensor2.TabIndex = 2;
+            this.pbSensor2.TabStop = false;
+            // 
+            // pbTanque
+            // 
+            this.pbTanque.Location = new System.Drawing.Point(368, 92);
+            this.pbTanque.Name = "pbTanque";
+            this.pbTanque.Size = new System.Drawing.Size(131, 141);
+            this.pbTanque.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTanque.TabIndex = 2;
+            this.pbTanque.TabStop = false;
+            // 
+            // pbBrujula
+            // 
+            this.pbBrujula.Location = new System.Drawing.Point(543, 92);
+            this.pbBrujula.Name = "pbBrujula";
+            this.pbBrujula.Size = new System.Drawing.Size(168, 141);
+            this.pbBrujula.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBrujula.TabIndex = 0;
+            this.pbBrujula.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-3, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(733, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "_________________________________________________________________________________" +
+    "______________________-";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(285, 277);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 13);
+            this.lblFecha.TabIndex = 9;
+            // 
+            // cmdCambiarFecha
+            // 
+            this.cmdCambiarFecha.AutoSize = true;
+            this.cmdCambiarFecha.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.cmdCambiarFecha.Location = new System.Drawing.Point(99, 390);
+            this.cmdCambiarFecha.Name = "cmdCambiarFecha";
+            this.cmdCambiarFecha.Size = new System.Drawing.Size(82, 23);
+            this.cmdCambiarFecha.TabIndex = 10;
+            this.cmdCambiarFecha.Text = "Ajustar Fecha";
+            this.cmdCambiarFecha.UseVisualStyleBackColor = true;
+            this.cmdCambiarFecha.Click += new System.EventHandler(this.cmdCambiarFecha_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 532);
+            this.Controls.Add(this.cmdCambiarFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.txtNuevaFecha);
+            this.Controls.Add(this.txtdato);
+            this.Controls.Add(this.pbSensor1);
+            this.Controls.Add(this.pbSensor2);
+            this.Controls.Add(this.pbTanque);
+            this.Controls.Add(this.cmdAccion);
+            this.Controls.Add(this.pbBrujula);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSensor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSensor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTanque)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBrujula)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbBrujula;
+        private System.Windows.Forms.Button cmdAccion;
+        private System.Windows.Forms.PictureBox pbTanque;
+        private System.Windows.Forms.TextBox txtdato;
+        private System.Windows.Forms.PictureBox pbSensor2;
+        private System.Windows.Forms.PictureBox pbSensor1;
+        private System.Windows.Forms.TextBox txtNuevaFecha;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button cmdCambiarFecha;
+    }
+}
+
---
+++ Tarea1TableroCarro/Tarea1TableroCarro/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1TableroCarro
+{
+    public partial class Form1 : Form
+    {
+        short contadorProceso;   
+        
+        public Form1()
+        {   
+            InitializeComponent();
+        }
+
+       int Recorrer(int variable, int recorrer, int comparacion)
+        {
+           
+            variable >>= recorrer;
+             
+            contadorProceso++;
+            return variable & comparacion;
+        }
+
+        void Mostrar(short c, int informacion)
+        {
+            switch (c)
+            {
+                case 1:
+                    switch (informacion)
+                    {
+                        case 1:
+                            pbBrujula.Image = Properties.Resources.Norte;
+                            break;
+                        case 2:
+                            pbBrujula.Image = Properties.Resources.NorEste;
+                            break;
+                        case 3:
+                            pbBrujula.Image = Properties.Resources.Este;
+                            break;
+                        case 4:
+                            pbBrujula.Image = Properties.Resources.SurEste;
+                            break;
+                        case 5:
+                            pbBrujula.Image = Properties.Resources.Sur;
+                            break;
+                        case 6:
+                            pbBrujula.Image = Properties.Resources.SurOeste;
+                            break;
+                        case 7:
+                            pbBrujula.Image = Properties.Resources.Oeste;
+                            break;
+                        case 8:
+                            pbBrujula.Image = Properties.Resources.NorOeste;
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (informacion)
+                    {
+                        case 0:
+                            pbTanque.Image = Properties.Resources.TanqueGasolina;
+                            break;
+                        case 1:
+                            pbTanque.Image = Properties.Resources.TanqueMedio;
+                            break;
+                        case 2:
+                            pbTanque.Image = Properties.Resources.TanqueFull;
+                            break;
+                        case 3:
+                            pbTanque.Image = Properties.Resources.TanqueLlenandose;
+                            break;
+                    }
+                    break;
+                case 3:
+                    if (informacion == 0)
+                    {
+                        pbSensor2.Image = Properties.Resources.Alerta_de_temperatura;
+                    }
+                    else
+                    {
+                        pbSensor2.Image = Properties.Resources.noAgua;
+                    }
+                    break;
+                case 4:
+                    if(informacion == 0)
+                    {
+                        pbSensor1.Image = Properties.Resources.tablero_luces_cinturon;
+                    }
+                    else
+                    {
+                        pbSensor1.Image = Properties.Resources.Mal_funcionamiento_del_motor;
+                    }
+                    break;
+            }
+        }
+
+        private void cmdAccion_Click(object sender, EventArgs e)
+        {
+            int dia = 0;
+            int mes = 0;
+            int año = 0;
+            Int32 dato = Convert.ToInt32(txtdato.Text);
+            int info;
+           info = Recorrer(dato, 1, 7); //Recorrer el primer bit y compara con 7
+           Mostrar(contadorProceso, info);//Mostrar la direccion
+           info = Recorrer(dato, 3, 3);//Recorrer 3 bits y comparar con 3
+           Mostrar(contadorProceso, info);//Muestra el estado del tanque
+           info = Recorrer(dato, 2, 3);//Recorrer  2 bits y comparar con 3
+           Mostrar(contadorProceso, info);//Mostrar el estado del sensor 1
+           info = Recorrer(dato, 1, 1);//Recorrer 1 bit y comparar con 1
+           Mostrar(contadorProceso, info);//Mostrar el estado del sensor 2
+                                          // textBox1.Text = Convert.ToString(info);
+
+            // dateTimePicker1.Value.Year();
+            //info = Recorrer(dato, )
+            dia = Recorrer(dato, 1, 31); //Empezamos con los dias
+            mes = Recorrer(dato, 5, 15);
+            año = Recorrer(dato, 4, 255) + 1900;
+            lblFecha.Text += dia + "/" + mes + "/" + año;
+            
+            //dtpFecha.Value.Year(info);
+
+           contadorProceso = 0;
+
+        }
+
+        private void cmdCambiarFecha_Click(object sender, EventArgs e)
+        {
+            int fechaTemp = dtpFecha.Value.Year;
+
+            fechaTemp -= 1900;
+            fechaTemp <<= 4;
+            fechaTemp += dtpFecha.Value.Month;
+            
+            fechaTemp <<= 5;
+            fechaTemp += dtpFecha.Value.Day;
+           
+            //fecha -= 1900;
+            //fecha += fechaTemp;
+            txtNuevaFecha.Text = Convert.ToString(fechaTemp);
+
+
+        }
+    }
+}
